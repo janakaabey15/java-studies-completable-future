9d3517a7c6bafcf7d67984eba2f5dc80919c2d8a
Janaka Abeytunge <janakaabeytunge@Janakas-MacBook-Pro.local>
2023-11-05 20:57:11 +0100

    CF test

---
+++ src/main/java/com/learnjava/completableFuture/CompletableFutureHelloWorld.java
@@ -0,0 +1,24 @@
+package com.learnjava.completableFuture;
+
+import com.learnjava.service.HelloWorldService;
+import com.learnjava.util.LoggerUtil;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.learnjava.util.CommonUtil.delay;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class CompletableFutureHelloWorld {
+
+    public static void main(String[] args) {
+
+        HelloWorldService helloWorldService = new HelloWorldService();
+        CompletableFuture.supplyAsync(()->helloWorldService.helloWorld())
+                .thenAccept((result)->{
+                    log("Result is " + result);
+                }).join();
+
+        log("Done!");
+       // delay(2000);
+    }
+}
